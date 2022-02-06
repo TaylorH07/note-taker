f06a73df1cfcb14887e24aae4b96ea1ba4bfab1f
taylor.holmes <taylorh2343@gmail.com>
2022-02-06 00:19:51 -0600

    added get and post routes

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,36 @@
 const router = require('express').Router();
-const db = require('../db/db.js')
+//const db = require('../db/db.js');
+const { v4: uuidv4 } = require('uuid');
+const { response } = require('express');
 
+// GET router
 router.get('/notes', (req,res) => {
-    res.status(200).json(JSON.parse(db.getNotes()));
-})
+    //res.status(200).json(JSON.parse(db.getNotes()));
 
+    const getData = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+
+    res.json(getData);
+});
+
+// POST router 
 router.post('/notes', (req, res) => {
-    const note = req.body;
-    db.postNotes(note);
+    //new note from request body
+    const newEntry = req.body;
+
+    //id obtained from uuid package
+    newEntry.id = uuidv4();
+
+    //read data in json
+    const getData = JSON.parse(fs.readFileSync("./db/db.json"));
+
+    //new entry in db.json file
+    fs.writeFileSync('./db/db.json', JSON.stringify(getData));
+
+    //sends data response
+    response.json(getData);
+
     // wait for this : res.status(200).send('wrote file successfully')
-    console.log(note);
-})
+    //console.log(newEntry);
+});
 
 module.exports = router;
