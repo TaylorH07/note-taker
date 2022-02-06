ac9c99100e47a49f6deff5e27e67c53a85ca65d0
taylor.holmes <taylorh2343@gmail.com>
2022-02-06 02:21:57 -0600

    added delete router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
-//const db = require('../db/db.js');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const { response } = require('express');
+const db = require('../db/db.js');
+
 
 // GET router
-router.get('/notes', (req,res) => {
+router.get('/api/notes', (req,res) => {
     //res.status(200).json(JSON.parse(db.getNotes()));
 
-    const getData = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    let getData = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
 
     res.json(getData);
 });
 
 // POST router 
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     //new note from request body
     const newEntry = req.body;
 
@@ -21,7 +23,7 @@ router.post('/notes', (req, res) => {
     newEntry.id = uuidv4();
 
     //read data in json
-    const getData = JSON.parse(fs.readFileSync("./db/db.json"));
+    let getData = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
 
     //new entry in db.json file
     fs.writeFileSync('./db/db.json', JSON.stringify(getData));
@@ -32,6 +34,19 @@ router.post('/notes', (req, res) => {
     // wait for this : res.status(200).send('wrote file successfully')
     //console.log(newEntry);
 });
+
+//DELETE router
+// router.delete('/api/notes/:id', (req, res) => {
+//     let notesId = req.params.id.toString();
+
+//     let getData = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+
+//     const newEntry = getData.filter( note => note.id.toString() !== notesId );
+
+//     fs.writeFileSync('./db/db.json', JSON.stringify(newEntry));
+
+//     response.json(newEntry);
+// });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
+
+//const fs = require('fs');
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
-app.use(htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT,  () => {
-    console.log("App running on port" + PORT);
+    console.log(`API server now on port ${PORT}!`);
 });
